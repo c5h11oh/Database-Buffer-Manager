cb829032f1dfa3a456b0dc5478d44af3d899e9eb
Sven Hwang <whwang8@wisc.edu>
2021-03-29 00:14:23 -0500

    make a test8() but does not work

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 #include <stdlib.h>
 //#include <stdio.h>
 #include <cstring>
@@ -23,6 +25,7 @@
 #include "exceptions/page_not_pinned_exception.h"
 #include "exceptions/page_pinned_exception.h"
 #include "exceptions/buffer_exceeded_exception.h"
+#include "exceptions/insufficient_space_exception.h"
 
 #define PRINT_ERROR(str) \
 { \
@@ -39,7 +42,7 @@ RecordId rid[num], rid2, rid3;
 Page *page, *page2, *page3;
 char tmpbuf[100];
 BufMgr* bufMgr;
-File *file1ptr, *file2ptr, *file3ptr, *file4ptr, *file5ptr;
+File *file1ptr, *file2ptr, *file3ptr, *file4ptr, *file5ptr, *svenptr;
 
 void test1();
 void test2();
@@ -47,6 +50,7 @@ void test3();
 void test4();
 void test5();
 void test6();
+void test8();
 void testBufMgr();
 
 int main() 
@@ -125,6 +129,7 @@ void testBufMgr()
   const std::string& filename3 = "test.3";
   const std::string& filename4 = "test.4";
   const std::string& filename5 = "test.5";
+//   const std::string& filenamesven = "test.sven";
 
   try
 	{
@@ -133,6 +138,9 @@ void testBufMgr()
     File::remove(filename3);
     File::remove(filename4);
     File::remove(filename5);
+	File::remove("test.sven1");
+	File::remove("test.sven2");
+
   }
 	catch(const FileNotFoundException &e)
 	{
@@ -143,12 +151,14 @@ void testBufMgr()
 	File file3 = File::create(filename3);
 	File file4 = File::create(filename4);
 	File file5 = File::create(filename5);
+	// File filesven = File::create(filenamesven);
 
 	file1ptr = &file1;
 	file2ptr = &file2;
 	file3ptr = &file3;
 	file4ptr = &file4;
 	file5ptr = &file5;
+	// svenptr = &filesven;
 
 	//Test buffer manager
 	//Comment tests which you do not wish to run now. Tests are dependent on their preceding tests. So, they have to be run in the following order. 
@@ -159,6 +169,12 @@ void testBufMgr()
 	test4();
 	test5();
 	test6();
+	
+	delete bufMgr;
+	bufMgr = new BufMgr(num);
+	file1.~File();
+	test8();
+	file1 = File::open(filename1);
 
 	//Close files before deleting them
 	file1.~File();
@@ -173,6 +189,7 @@ void testBufMgr()
 	File::remove(filename3);
 	File::remove(filename4);
 	File::remove(filename5);
+	// File::remove(filenamesven);
 
 	delete bufMgr;
 
@@ -333,6 +350,108 @@ void test6()
 	bufMgr->flushFile(file1ptr);
 }
 
-void test8(){
-
+int hashFunction(int in, int size = 8193){
+	return (in * 21017 + 4027) % size;
 }
+
+inline bool same(std::istream& in1, std::istream& in2){
+	in1.seekg(0, std::istream::beg);
+	in2.seekg(0, std::istream::beg);
+	return std::equal(std::istreambuf_iterator<char>(in1.rdbuf()),
+							std::istreambuf_iterator<char>(), 
+							std::istreambuf_iterator<char>(in2.rdbuf()));
+}
+
+std::string blankStr("");
+void assertSame(std::istream& in1, std::istream& in2, std::string&& err_msg = "", bool diff = false){
+	bool result = same(in1, in2);
+	if(diff && result){
+		PRINT_ERROR( ("ERROR: SVEN: file should be different but it's same. MSG:" + err_msg) );
+	}
+	else if(!result){
+		PRINT_ERROR( ("ERROR: SVEN: file should be same but it's different. MSG:" + err_msg) );
+	}
+}
+
+void test8(){
+	// testing the persistence is really done
+		// Copy test.1 to test.sven1,2
+		// std::ifstream  src("test.1", std::ios::binary);
+		// std::fstream  dst1("test.sven1", std::ios::binary | std::ios::trunc);
+		// std::fstream  dst2("test.sven2", std::ios::binary | std::ios::trunc);
+		// src.seekg(0, std::ifstream::beg);
+		// dst1 << src.rdbuf();
+		// src.seekg(0, std::ifstream::beg);
+		// dst2 << src.rdbuf();
+		/* https://stackoverflow.com/questions/10195343/copy-a-file-in-a-sane-safe-and-efficient-way */
+		
+		// Make sure they're identical 
+		std::fstream sven1("test.sven1", std::ios::binary), 
+					 sven2("test.sven2", std::ios::binary);
+		// assertSame(src, sven1);
+		// assertSame(src, sven2);
+		// src.close();
+		/* https://stackoverflow.com/questions/6163611/compare-two-files/37575457 */
+		
+		File filesven1 = File::open("test.sven1");
+		File filesven2 = File::open("test.sven2");
+		
+	// 1. sven1: if there are 100 pages edited, the underlying file will not change 
+	//	  because the buffer has size 100 pages
+		for(uint i = 0; i < num; ++i){
+			bufMgr->allocPage(&filesven1, pid[i], page);
+		}
+		sven1.seekg(0, std::ifstream::beg);
+		sven2.seekg(0, std::ifstream::beg);
+		sven2 << sven1.rdbuf();
+		for(uint i = 0; i < num; ++i){
+			sprintf((char*)tmpbuf, "test.sven1 Page %u hash %d", pid[i], hashFunction(i));
+			rid[i] = page->insertRecord(tmpbuf);
+			bufMgr->unPinPage(&filesven1, pid[i], true);
+		}
+		assertSame(sven1, sven2, "SVEN1");
+	
+	// 2. if we call flush, the data will write into the file
+		bufMgr->flushFile(&filesven1);
+		assertSame(sven1, sven2, "SVEN2", true); // I don't know how to do better
+	
+	PageId bigPid[2 * num];
+	RecordId bigRid[2 * num];
+	// 3. sven2: if we edit 200 distinct pages, the first 100 will be written in the file
+		for(uint i = 0; i < 2 * num; ++i){
+			bufMgr->allocPage(&filesven2, bigPid[i], page);
+			sprintf((char*)tmpbuf, "test.sven2 Page %u hash %d", bigPid[i], hashFunction(i));
+			bigRid[i] = page->insertRecord(tmpbuf);
+			bufMgr->unPinPage(&filesven2, bigPid[i], true);
+		}
+		assertSame(sven1, sven2, std::string("SVEN3"));
+	// 4. if we delete bufMgr, the deconstructor will be called and the file will be written
+		delete bufMgr;
+		bufMgr = new BufMgr(num);
+		for(uint i = 0; i < 2 * num; ++i){
+			bufMgr->readPage(&filesven2, bigPid[i], page);
+			sprintf((char*)tmpbuf, "test.sven2 Page %u hash %d", bigPid[i], hashFunction(i));
+			if(strncmp(page->getRecord(bigRid[i]).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
+			{
+				PRINT_ERROR("ERROR :: CONTENTS DID NOT MATCH. SVEN4");
+			}			
+			bufMgr->unPinPage(&filesven2, bigPid[i], false);
+		}
+	// 5. the page should eventually be filled and throw InsufficientSpaceException if we continue to write record
+		try{
+			bufMgr->readPage(&filesven2, bigPid[1], page);
+			for(uint i = 0; i < UINT32_MAX; ++i){
+				page->insertRecord("Matt, Ismael, and Sven are all handsome guy");
+			}
+		}
+		catch(const InsufficientSpaceException& e){
+			return;
+		}
+		
+		PRINT_ERROR("ERROR :: EXPECT AN InsufficientSpaceException. SVEN5");
+
+	// close istream
+	sven1.close();
+	sven2.close();
+}
+
